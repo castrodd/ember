7d77aba2cc5c5fc343f05d5c86291ef7eb5a77a0
Daniel Castro <castrodd@gmail.com>
2017-09-20 12:14:59 -0400

    Created models

---
+++ app/models/witness.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  fname: DS.attr('string'),
+  lname: DS.attr('string'),
+  email: DS.attr('string'),
+  sightings: DS.hasMany('sighting')
+});
--- app/routes/sightings.js
+++ app/routes/sightings.js
@@ -2,32 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-        id: 1,
-        location: 'Asilomar',
-        sightedAt: new Date('2016-07-03')
-      },
-      {
-        id: 2,
-        location: 'Asilomar',
-        sightedAt: new Date('2016-07-04')
-      },
-      {
-        id: 3,
-        location: 'Marosila',
-        sightedAt: new Date('2016-07-05')
-      },
-      {
-        id: 4,
-        location: 'Marosila',
-        sightedAt: new Date('2016-07-06')
-      },
-      {
-        id: 5,
-        location: 'Rosamila',
-        sightedAt: new Date('2016-07-07')
-      }
-    ];
+    let record1 = this.store.createRecord('sighting', {
+      location: 'Atlanta',
+      sightedAt: new Date('2016-07-04')
+    });
+    let record2 = this.store.createRecord('sighting', {
+      location: 'Philadelphia',
+      sightedAt: new Date('2016-09-14')
+    });
+    let record3 = this.store.createRecord('sighting', {
+      location: 'Baltimore',
+      sightedAt: new Date('2016-02-01')
+    });
+
+    return [record1, record2, record3];
   }
 });
