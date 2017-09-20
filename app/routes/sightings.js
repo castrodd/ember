39d4b5212963e981c7af2c283d76c15a1f2175e9
Daniel Castro <castrodd@gmail.com>
2017-09-20 11:55:36 -0400

    Test data and landing page

--- app/routes/sightings.js
+++ app/routes/sightings.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      {
+        id: 1,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-07-03')
+      },
+      {
+        id: 2,
+        location: 'Asilomar',
+        sightedAt: new Date('2016-07-04')
+      },
+      {
+        id: 3,
+        location: 'Marosila',
+        sightedAt: new Date('2016-07-05')
+      },
+      {
+        id: 4,
+        location: 'Marosila',
+        sightedAt: new Date('2016-07-06')
+      },
+      {
+        id: 5,
+        location: 'Rosamila',
+        sightedAt: new Date('2016-07-07')
+      }
+    ];
+  }
 });
