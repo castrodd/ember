8e761cb1e0ce9e61061484da273008bf76228878
Daniel Castro <castrodd@gmail.com>
2017-09-22 11:24:15 -0400

    Rollover title, formatted date

--- app/helpers/moment-from.js
+++ app/helpers/moment-from.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function momentFrom(params) {
   var time = window.moment(...params);
-  var formatted = time.fromNow();
+  var formatted = time.format("dddd, MMMM Do YYYY");
   return new Ember.String.htmlSafe(
     '<span class="text-primary">' + formatted + '</span>'
   );
