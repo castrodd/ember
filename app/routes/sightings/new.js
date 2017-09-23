5973995189b5a1748b030998da732e1e62b3d229
Daniel Castro <castrodd@gmail.com>
2017-09-22 20:26:56 -0400

    Fixed controller error

--- app/routes/sightings/new.js
+++ app/routes/sightings/new.js
@@ -7,5 +7,30 @@ export default Ember.Route.extend({
       cryptids: this.store.findAll('cryptid'),
       witnesses: this.store.findAll('witness')
     });
+  },
+  sighting: Ember.computed.alias('controller.model.sighting'),
+  actions: {
+    willTransition() {
+      var sighting = this.get('controller.model.sighting');
+      if (sighting.get('hasDirtyAttributes')) {
+        sighting.deleteRecord();
+      }
+    },
+    create() {
+      var self = this;
+      this.get('sighting').save().then(function() {
+        self.transitionTo('sightings');
+      });
+    },
+    cancel() {
+      this.get('sighting').deleteRecord();
+      this.transitionTo('sightings');
+    },
+    didMakeWitnessSelection(value) {
+      this.get('sighting').set('witnesses', value);
+    },
+   didMakeCryptidSelection(value) {
+      this.get('sighting').set('cryptid', value);
+    }
   }
 });
